30b0466bb26ce05d3f8ebb91cc22b4cf40996580
David Madl <git@abanbytes.eu>
2017-03-05 03:30:59 +0400

    look at beat detection

--- hsh_beatdet/__init__.py
+++ hsh_beatdet/__init__.py
@@ -1,2 +1,35 @@
+import numpy as np
 from ML.ppg_beatdetector_v1 import getrr_v1
 from ML.ppg_beatdetector_v2 import getrr_v2
+from hsh_signal.heartseries import HeartSeries
+from hsh_signal.signal import highpass
+
+
+def beatdet_getrr_v2(data):
+    ibis, filtered, idx = getrr_v2(data, fps=30, convert_to_ms=True)
+    return ibis, filtered, idx
+
+
+def beatdet_getrr_v1(data):
+    series = data[:,1]
+    reversed_data = np.vstack((data[:,0], list(reversed(series)))).T
+    ibis, filtered, idx = getrr_v1(reversed_data, fps = 30, convert_to_ms=True)
+    ibis = np.array(list(reversed(ibis)))
+    idx = list((len(series)-1) - np.array(list(reversed(idx))))
+    filtered = np.array(list(reversed(filtered)))
+    return ibis, filtered, idx
+
+
+def beatdet(data, beat_detector, fps=30):
+    times, series = data[:,0], data[:,1]
+
+    if np.std(series) < 1e-6:
+        # if np.std(series)==0, getrr() will raise "ValueError: array must not contain infs or NaNs"
+        # instead, return no beats == []
+        return HeartSeries(series, [], fps=fps, lpad=-times[0]*fps)
+
+    ibis, filtered, idx = beat_detector(data)
+
+    hp_series = highpass(highpass(series, fps), fps)
+
+    return HeartSeries(hp_series, idx, fps=fps, lpad=-times[0]*fps)
---
+++ test.py
@@ -0,0 +1,24 @@
+from hsh_signal.app_parser import AppData
+import matplotlib.pyplot as plt
+from hsh_beatdet import beatdet_getrr_v1, beatdet_getrr_v2, beatdet
+import random
+from os.path import basename
+
+
+if __name__ == '__main__':
+    measurements = AppData.list_measurements()
+
+    #for ad in measurements:
+    for i in range(100):
+        ad = random.choice(measurements)
+        mf = basename(ad.meta_filename)
+        print mf
+
+        #ppg = ad.ppg_parse_beatdetect('getrr')
+        data = ad.ppg_data()
+
+        rr1 = beatdet(data, beatdet_getrr_v1)
+        rr2 = beatdet(data, beatdet_getrr_v2)
+
+        rr2.plot()
+        plt.show()
